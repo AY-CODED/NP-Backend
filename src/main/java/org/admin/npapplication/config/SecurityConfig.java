c62e6b6d79f661b836ea6aa2fe8253a597c116d6
AY-CODED <ayomiposoyinka@gmail.com>
2026-07-13 09:40:28 +0100

    change

--- src/main/java/org/admin/npapplication/config/SecurityConfig.java
+++ src/main/java/org/admin/npapplication/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "https://nugesphramacy.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
         config.setAllowCredentials(true);
