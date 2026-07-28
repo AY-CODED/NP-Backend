0b16eb02e2a1830ac7e17c0c193231da63051dbd
Michael Olatunji <molatunji371@gmail.com>
2026-07-28 22:43:26 +0100

    Update SecurityConfig.java

--- src/main/java/org/admin/npapplication/config/SecurityConfig.java
+++ src/main/java/org/admin/npapplication/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "https://nugesphramacy.vercel.app/", "https://np-admin-one.vercel.app/"));
+        config.setAllowedOrigins(List.of("https://nugesphramacy.vercel.app/", "https://np-admin-one.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
         config.setAllowCredentials(true);
